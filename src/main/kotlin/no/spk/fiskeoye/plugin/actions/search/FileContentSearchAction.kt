1b3df138534da27555599b9cefeeed53309b8f1f
Mohamed Adrik <mohamed.adrik@spk.no>
2025-06-25 12:14:13 +0200

    Add fiskeoye search to ProjectViewPopupMenu (#43)
    * Add fiskeoye search to ProjectViewPopupMenu
    
    * Bump version

--- src/main/kotlin/no/spk/fiskeoye/plugin/actions/search/FileContentSearchAction.kt
+++ src/main/kotlin/no/spk/fiskeoye/plugin/actions/search/FileContentSearchAction.kt
@@ -1,12 +1,12 @@
-package no.spk.fiskeoye.plugin.actions.editor
+package no.spk.fiskeoye.plugin.actions.search
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import no.spk.fiskeoye.plugin.util.getFileContentPanel
 
-internal class FileContentEditorSearchAction : EditorSearchAction() {
+internal class FileContentSearchAction : SearchAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
-        val selectedText = getCurrentCaret(e)?.selectedText ?: return
+        val selectedText = getCurrentCaret(e)?.selectedText ?: (getCurrentFile(e)?.nameWithoutExtension ?: return)
         val fileContentPanel = getFileContentPanel()
         show(e, fileContentPanel)
         fileContentPanel.apply {
--- src/main/kotlin/no/spk/fiskeoye/plugin/actions/search/SearchAction.kt
+++ src/main/kotlin/no/spk/fiskeoye/plugin/actions/search/SearchAction.kt
@@ -1,18 +1,21 @@
-package no.spk.fiskeoye.plugin.actions.editor
+package no.spk.fiskeoye.plugin.actions.search
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.editor.Caret
+import com.intellij.openapi.vfs.VirtualFile
 import no.spk.fiskeoye.plugin.actions.FiskeoyeAction
 import no.spk.fiskeoye.plugin.ui.FiskeoyePanel
 import no.spk.fiskeoye.plugin.util.getToolWindow
 
-internal abstract class EditorSearchAction : FiskeoyeAction() {
+internal abstract class SearchAction : FiskeoyeAction() {
 
     protected fun getCurrentCaret(e: AnActionEvent): Caret? = e.getData(CommonDataKeys.EDITOR)?.caretModel?.currentCaret
 
+    protected fun getCurrentFile(e: AnActionEvent): VirtualFile? = e.getData(CommonDataKeys.VIRTUAL_FILE)
+
     override fun update(e: AnActionEvent) {
-        e.presentation.isEnabledAndVisible = getCurrentCaret(e)?.hasSelection() ?: false
+        e.presentation.isEnabledAndVisible = getCurrentCaret(e)?.hasSelection() ?: false || e.getData(CommonDataKeys.VIRTUAL_FILE)?.isValid ?: false
     }
 
     protected fun show(e: AnActionEvent, fiskeoyePanel: FiskeoyePanel) {
